2de42654501b739094200eaa58f56362448b4992
hao wang <hao.wang@mdpi.com>
2025-09-19 17:09:38 +0800

    init

---
+++ README.md
@@ -0,0 +1,1 @@
+Learning plotly offical tutorial.
---
+++ myscript.js
@@ -0,0 +1,10 @@
+import Plotly from 'plotly.js-dist';
+
+const TESTER = document.getElementById('tester');
+Plotly.newPlot( TESTER, [{
+    
+    x: [1, 2, 3, 4, 5],
+    
+    y: [1, 2, 4, 8, 16] }], {
+        
+        margin: { t: 0 } } );
