de26d525f66d409cc348e844d8aead157848e96b
hao wang <hao.wang@mdpi.com>
2025-09-28 09:41:02 +0800

    dynamic imports

--- myscript.js
+++ myscript.js
@@ -1,31 +1,8 @@
+async function loadFeature(name) {
+  if (name === "scroll_zoom") {
+    const { showFeature } = await import("./src/scroll_zoom.js");
+    showFeature();
+  }
+}
 
-
-import Plotly from 'plotly.js-dist';
-
-// Example: y = x^2 and y = sin(x)
-const x = Array.from(
-  {length: 100},
-  (_, i) => -5 + i * 0.1
-);
-
-const trace1 = {
-  x: x,
-  y: x.map(v => v*v),
-  mode: 'lines',
-  name: 'y = x^2'
-};
-
-const trace2 = {
-  x: x,
-  y: x.map(v => Math.sin(v)),
-  mode: 'line',
-  name: 'y = sin(x)'
-};
-
-const layout = {
-  titile: "Graphing Curves with Plotly.js",
-  xaxis: {title: 'x'},
-  yaxis: { title: 'y'}
-};
-
-Plotly.newPlot('tester', [trace1, trace2], layout);
+document.getElementById("scroll_zoom_btn").onclick = () => loadFeature("scroll_zoom");
--- src/scroll_zoom.js
+++ src/scroll_zoom.js
@@ -1,18 +1,20 @@
 import Plotly from 'plotly.js-dist';
 
-let tracel = {
-  x: ['2020-10-04', '2021-11-04', '2023-12-04'],
-  y: [90, 40, 60],
-  type: 'scatter'
-};
-
-let data = [tracel];
-
-let layout = {
-  title: {
-    text: 'Scroll and Zoom'
-  },
-  showlegend: false
-};
-
-Plotly.newPlot('tester', data, layout, {scrollZoom: true});
+export function showFeature() {
+  let tracel = {
+    x: ['2020-10-04', '2021-11-04', '2023-12-04'],
+    y: [90, 40, 60],
+    type: 'scatter'
+  };
+  
+  let data = [tracel];
+  
+  let layout = {
+    title: {
+      text: 'Scroll and Zoom'
+    },
+    showlegend: false
+  };
+  
+  Plotly.newPlot('tester', data, layout, {scrollZoom: true});
+}
